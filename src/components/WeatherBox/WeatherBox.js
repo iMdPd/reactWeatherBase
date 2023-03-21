6c7765b18a6e22e87c1417df4d118f6bb3eac3a5
Mariusz Politowicz <politowiczmar@gmail.com>
2023-03-21 19:26:42 +0100

    fetch weatcher data from api

--- src/components/WeatherBox/WeatherBox.js
+++ src/components/WeatherBox/WeatherBox.js
@@ -5,7 +5,11 @@ import { useCallback } from "react";
 
 const WeatherBox = (props) => {
   const handleCityName = useCallback((city) => {
-    console.log(city);
+    const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=3745acc32b2f5da91791064011ecabbd&units=metric`;
+
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => console.log(data));
   });
 
   return (
