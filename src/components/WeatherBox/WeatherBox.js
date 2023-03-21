0999da4dbe91cdf021edba8b59b39656b1479b99
Mariusz Politowicz <politowiczmar@gmail.com>
2023-03-21 21:09:53 +0100

    Throw Error component on status != 200

--- src/components/WeatherBox/WeatherBox.js
+++ src/components/WeatherBox/WeatherBox.js
@@ -2,35 +2,43 @@ import PickCity from "../PickCity/PickCity";
 import WeatherSummary from "../WeatherSummary/WeatherSummary";
 import Loader from "../Loader/Loader";
 import { useCallback, useState } from "react";
+import ErrorBox from "../ErrorBox/ErrorBox";
 
 const WeatherBox = (props) => {
   const [weatherData, setWeatherData] = useState();
   const [pending, setPending] = useState(false);
+  const [error, setError] = useState(false);
 
   const handleCityName = useCallback((city) => {
     const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=3745acc32b2f5da91791064011ecabbd&units=metric`;
 
     setPending(true);
-    fetch(url)
-      .then((response) => response.json())
-      .then((data) => {
-        const payload = {
-          city: data.name,
-          temp: data.main.temp,
-          icon: data.weather[0].icon,
-          description: data.weather[0].main,
-        };
+    fetch(url).then((response) => {
+      if (response.status === 200) {
+        setError(false);
+        return response.json().then((data) => {
+          const payload = {
+            city: data.name,
+            temp: data.main.temp,
+            icon: data.weather[0].icon,
+            description: data.weather[0].main,
+          };
 
-        setWeatherData(payload);
-        setPending(false);
-      });
+          setWeatherData(payload);
+          setPending(false);
+        });
+      } else {
+        setError(true);
+      }
+    });
   });
 
   return (
     <section>
       <PickCity action={handleCityName} />
       {weatherData && pending === false && <WeatherSummary {...weatherData} />}
-      {pending === true && <Loader />}
+      {error === true && <ErrorBox>Country doesn't exist</ErrorBox>}
+      {pending === true && error === false && weatherData && <Loader />}
     </section>
   );
 };
