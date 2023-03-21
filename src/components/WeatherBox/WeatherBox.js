aa3d677bc170b3ea441988d509cda2b18d5694e6
Mariusz Politowicz <politowiczmar@gmail.com>
2023-03-21 19:37:01 +0100

    rename fetch data

--- src/components/WeatherBox/WeatherBox.js
+++ src/components/WeatherBox/WeatherBox.js
@@ -1,24 +1,29 @@
 import PickCity from "../PickCity/PickCity";
 import WeatherSummary from "../WeatherSummary/WeatherSummary";
 import Loader from "../Loader/Loader";
-import { useCallback } from "react";
+import { useCallback, useState } from "react";
 
 const WeatherBox = (props) => {
+  const [weatherData, setWeatherData] = useState();
+
   const handleCityName = useCallback((city) => {
     const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=3745acc32b2f5da91791064011ecabbd&units=metric`;
 
     fetch(url)
       .then((response) => response.json())
       .then((data) => {
-        const weatherData = {
+        const payload = {
           city: data.name,
           temp: data.main.temp,
           icon: data.weather[0].icon,
           description: data.weather[0].main,
         };
-        console.log(weatherData);
+
+        setWeatherData(payload);
       });
   });
+
+ 
 
   return (
     <section>
